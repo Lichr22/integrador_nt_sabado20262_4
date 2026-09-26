44b3357ccd1d7890cd9980247742fdd5cccdac6c
Valentina Corredor <jvalecorredor@gmail.com>
2026-09-26 10:36:07 -0500

    Feat: Agregué funcion generadora de datos y la probamos

--- simulacion_categorias.py
+++ simulacion_categorias.py
@@ -9,6 +9,7 @@ OJO: `area_responsable` NO esta en el modelo de Backend II: es una columna EXTRA
 '''
 import random
 import uuid
+import pandas as pd
 from faker import Faker
 
 #1. Sembrar semillas para los datos a simular
@@ -20,3 +21,27 @@ random.seed(42)
 # descripcion (texto),
 # area_responsable (texto)
 
+#3 Establecer una constante para EL NUMERO DE SIMULACIONES
+FILAS=250
+CATEGORIAS=[]
+AREAS=["Marketing", "Ventas", "Desarrollo", "Soporte"]
+FALSITO = Faker("es_CO")
+
+
+#4. Funcion generadora
+def generar_datos(numero_filas=250):
+    categorias=[]
+    for _ in range(numero_filas):
+        categorias.append({
+            "id":str(uuid.uuid4()),
+            "nombre":FALSITO.name(),
+            "descripcion":FALSITO.sentence(nb_words=8),
+            "area_responsable":random.choice(AREAS),
+        })
+    return categorias
+
+#5. Convirtiendo los datos generados en un dataframe con PANDAS
+tabla_ordenada_categorias=pd.DataFrame(generar_datos())
+
+#6. Probar la funcion
+print(tabla_ordenada_categorias)
--- simulacion_usuario.py
+++ simulacion_usuario.py
@@ -1,6 +1,8 @@
 import random
 import uuid
+import pandas as pd
 from faker import Faker
+
 
 #1. Sembrar semillas para los datos a simular
 random.seed(42)
@@ -9,12 +11,36 @@ Faker.seed(42)
 #2. Identificar los datos a simular con su tipo de dato
 #id (texto (UUID)),
 # nombre (texto)*****,
+# correo (texto),
+#contraseña_hash (texto),
+# rol (texto),
+# activo (booleano),
+# fecha_registro (fecha),
 # descripcion (texto),
 # area_responsable (texto)*****
 
 #3. Establecer una constante para EL NUMERO DE SIMULACIONES
 FILAS=250
+ROLES=["administrador","empresario","estudiante","profesor"]
+FALSITO = Faker("es_CO")
 
-#4. Funcion generadorea
-def generar_datos(numero_filas):
-    pass
+#4. Funcion generadora
+def generar_datos(numero_filas=400):
+    usuarios=[]
+    for _ in range(numero_filas):
+        usuarios.append({
+            "id":str(uuid.uuid4()),
+            "nombre":FALSITO.name(),
+            "correo":FALSITO.email(),
+            "contraseña_hash":FALSITO.sha256(),
+            "activo":random.choice([True,False]),
+            "rol":random.choice(ROLES),
+            "fecha_registro":FALSITO.date_time_between(start_date="-2y",end_date="now"),
+        })
+    return usuarios
+
+#5. Convirtiendo los datos generado en un dataframe con PANDAS
+tabla_ordenada_usuarios=pd.DataFrame(generar_datos())
+
+#6. Probar la funcion
+print(tabla_ordenada_usuarios)
